e82b52971aece910eceb246835574f74c0384a05
sevil khojasteh <sevilkhojasteh@sevils-MacBook-Air.local>
2026-05-27 21:34:41 +0200

    initial commit

--- introduction.py
+++ introduction.py
@@ -1,5 +1,6 @@
 # this is how to push to github
 # git add .
-# git commit -m "first commit"
-# git push -u origin master
+# git commit -m "Your descriptive message here"
+# git push origin main
+
 
