8fd399c612f9e28c6c4cd8c473846e7a1dae07c6
sevil khojasteh <sevilkhojasteh@sevils-MacBook-Air.local>
2026-05-28 00:56:48 +0200

    practice commit

--- environment_setup.py
+++ environment_setup.py
@@ -1,1 +1,51 @@
-print("Hello, World!")
+# IDE is the place you write the code, edit, and manage your project files. It provides features like code completion, debugging, and version control integration to enhance your coding experience.
+# IDE stands for Integrated Development Environment. It is a software application that provides comprehensive facilities to computer programmers for software development. An IDE typically includes a code editor, a compiler or interpreter, and a debugger that the
+
+"""
+Package manager?
+You use packages written by other people, a package manager automates the entire process of managing these libraries, including installation, updates, and dependency management. It ensures that you have the correct versions of libraries and their dependencies, making it easier to maintain your project and avoid conflicts between different packages.
+
+"""
+
+"""
+How the file systems Works in UNIX_based systems?
+everything starts from root: /
+cd / --> goes to root
+
+Actions:
+ls --> list the content of the current directory
+ls -l --> list the content of the current directory in long format (detailed information)
+ls -a --> list all files, including hidden files (those starting with a dot)
+ls -la --> list all files in long format (detailed information, including hidden files)
+cd .. --> go to the parent directory
+cd ~ --> go to the home directory
+cd - --> go to the previous directory
+cd /path/to/directory --> go to a specific directory
+pwd --> print the current working directory
+
+mkdir directory_name --> create a new directory
+rmdir directory_name --> remove an empty directory
+rm -r directory_name --> remove a directory and its contents
+touch file_name --> create a new empty file or update the timestamp of an existing file
+rm file_name --> remove a file
+
+cp source_file destination_file --> copy a file
+cp -r source_directory destination_directory --> copy a directory and its contents
+mv source_file destination_file --> move or rename a file
+mv source_directory destination_directory --> move or rename a directory
+
+history --> show the command history
+clear --> clear the terminal screen
+
+wget url --> download a file from the specified URL
+
+curl url --> transfer data from or to a server, can be used to download files or interact with APIs
+
+
+"""
+
+"""
+One of the most well-known repositories is PyPI (Python Package Index), which is the official repository for Python packages. It hosts a vast collection of libraries and tools that can be easily installed using package managers like pip. Other popular repositories include npm for JavaScript, RubyGems for Ruby, and Maven Central for Java.
+
+"""
+
