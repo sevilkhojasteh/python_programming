bf390afde6bf7347450cec6b59879a6bd708601d
sevilkhojasteh <sevilkhojasteh@gmail.com>
2026-06-29 14:30:44 +0200

    learning OOP

--- Roadmap/student-1.py
+++ Roadmap/student-1.py
@@ -1,9 +1,5 @@
 class Student:
     def __init__(self, name, house, patronus = None):
-        if not name:
-            raise ValueError("Missing name")
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
         self.name = name
         self.house = house
         self.patronus = patronus
@@ -12,11 +8,46 @@ class Student:
 
     def __str__(self):
         return f"{self.name} from {self.house}"
+    
+    def charm(self):
+        if self.patronus == "Stag":
+            return "Stag"
+        elif self.patronus == "Otter":
+            return "Otter"
+        elif self.patronus == "Jack Russell terrier":
+            return "Jack Russell terrier"
+        else:
+            return "No patronus"
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+
+    @property
+    def house(self):
+        return self._house
+    
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+
+
 
 
 def main():
     student = get_student()
-    print(student)
+    print("Expecto Patronum!")
+    print(student.charm())
 
 def get_student():
     name = input("What's your name? ")
