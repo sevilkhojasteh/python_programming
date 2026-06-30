da40e694a2615e73e6e22a17d57b2629d69e1a4b
sevilkhojasteh <sevilkhojasteh@gmail.com>
2026-06-30 22:59:18 +0200

    OOP

--- Roadmap/vault.py
+++ Roadmap/vault.py
@@ -6,6 +6,12 @@ class Vault:
 
     def __str__(self):
         return f"{self.galleons} Galleons, {self.sickles} Sickles, {self.knuts} Knuts"
+    
+    def __add__(self, other):   
+        galleons = self.galleons + other.galleons
+        sickles = self.sickles + other.sickles
+        knuts = self.knuts + other.knuts
+        return Vault(galleons, sickles, knuts)
 
 
 
@@ -17,9 +23,11 @@ print(potter)
 weasley = Vault(25, 50, 100)
 print(weasley)
 
-galleons = potter.galleons + weasley.galleons
-sickles = potter.sickles + weasley.sickles
-knuts = potter.knuts + weasley.knuts
+# galleons = potter.galleons + weasley.galleons
+# sickles = potter.sickles + weasley.sickles
+# knuts = potter.knuts + weasley.knuts
 
-total = Vault(galleons, sickles, knuts)
+# total = Vault(galleons, sickles, knuts)
+
+total = potter + weasley
 print(total)
