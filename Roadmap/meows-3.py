91c48ead7dfa4cc0d130c37c019e11fc9fc16ef3
sevilkhojasteh <sevilkhojasteh@gmail.com>
2026-07-01 12:13:48 +0200

    mypy

--- Roadmap/meows-3.py
+++ Roadmap/meows-3.py
@@ -10,6 +10,14 @@
 # meow(number)
 
 def meow(n: int) ->str:
+    """
+    Meow n times.
+
+    :param n: Number of times to meow
+    :type n: int
+    :return: A string of n meows, one per line
+    :rtype: str
+    """
     return "meow\n" * n
 
 number: int = int(input("How many times do you want to meow? "))
