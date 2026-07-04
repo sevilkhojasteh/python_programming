9ae22708a086aee68f316c01f842676df59a5be2
sevilkhojasteh <sevilkhojasteh@gmail.com>
2026-07-04 13:52:37 +0330

    unpacking

--- Roadmap/unpack.py
+++ Roadmap/unpack.py
@@ -1,3 +1,12 @@
-first, last = input("What's your name? ").split(" ")
+# first, last = input("What's your name? ").split(" ")
 
-print(f"First name: {first}")
+# print(f"First name: {first}")
+
+def total(galleons, sickles, knuts):
+    return galleons * 493 + sickles * 29 + knuts
+
+# coins = [100, 50, 25]
+# print(total(*coins), "Knuts")
+
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
+print(total(**coins), "knuts")
